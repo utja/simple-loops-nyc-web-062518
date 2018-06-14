8cd7e3dfe3d084393b60089272e9a6901282abf3
Jon Ahn <ahn.jon.s@gmail.com>
2018-06-14 01:43:00 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
   loop do
-    counter +=1
+    counter += 1
     puts phrase
     if counter == number_of_times
       break
